130a4b5a9bda2fe3a87e2a413d17abc284cca3a7
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2021-01-11 23:15:13 +0200

    Added a check for ensure any user accessing the MainActivity is authenticated.
    Signed-off-by: Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>

--- app/src/main/java/gr/uom/socialmediaaggregator/ui/login/LoginActivity.java
+++ app/src/main/java/gr/uom/socialmediaaggregator/ui/login/LoginActivity.java
@@ -1,34 +1,25 @@
 package gr.uom.socialmediaaggregator.ui.login;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import gr.uom.socialmediaaggregator.R;
-import gr.uom.socialmediaaggregator.ui.login.LoginViewModel;
-import gr.uom.socialmediaaggregator.ui.login.LoginViewModelFactory;
 import gr.uom.socialmediaaggregator.ui.main.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
--- app/src/main/java/gr/uom/socialmediaaggregator/ui/main/MainActivity.java
+++ app/src/main/java/gr/uom/socialmediaaggregator/ui/main/MainActivity.java
@@ -32,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Check if the user accesses the activity is authenticated.
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
